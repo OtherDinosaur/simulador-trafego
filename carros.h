1fc8f4a5e613e8cb8f52cbf1fc74de59bd78ef81
Dorian Dayvid Gomes Feitosa <dorian.dayvid@aluno.ufca.edu.br>
2026-07-06 19:52:26 +0000

    carro já andando no mapa

--- carros.h
+++ carros.h
@@ -1,14 +1,39 @@
-void anda_carro(int *carroX, int *carroY, int direcao)
-{
-    mapa[*carroX][*carroY] = '-';
-    if (direcao == 11) {
-        (*carroY)++;
-    } else if (direcao == 12) {
-        (*carroY)--;
-    } else if (direcao == 21) {
-        (*carroX)++;
-    } else if (direcao == 22) {
-        (*carroX)--;
+
+
+typedef struct {
+    int id;
+    int lin;
+    int col;
+    int direcao; // 1X -> horizontal, 2X -> vertical, X1 -> direita/cima, X2 -> esquerda/baixo
+    int velocidade; // 1, 2 ou 4 ticks
+    int Ambulancia;
+    char anterior;
+    pthread_t thread;
+} Carro;
+
+Carro carros[15];
+
+void anda_carro(Carro *carro) {
+    if (carro->id == -1) {
+        return;
     }
-    mapa[*carroX][*carroY] = 'C';
+    mapa[carro->lin][carro->col] = carro->anterior;
+    if (carro->direcao == 11) {
+        carro->col++;
+    } else if (carro->direcao == 12) {
+        carro->col--;
+    } else if (carro->direcao == 21) {
+        carro->lin++;
+    } else if (carro->direcao == 22) {
+        carro->lin--;
+    }
+    if (carro->lin >= LINHAS - 1 || carro->col >= COLUNAS - 1 || carro->lin <= 1 || carro->col <= 1) {
+        printf("Carro %d saiu do mapa!\n", carro->id);
+        carro->id = -1; // Marca o carro como removido
+        mapa[carro->lin][carro->col] = carro->anterior;
+        exit(1);
+        
+    }
+    carro->anterior = mapa[carro->lin][carro->col];
+    mapa[carro->lin][carro->col] = 'C';
 }
--- main.c
+++ main.c
@@ -5,14 +5,30 @@
 #include "carros.h"
 
 
+pthread_mutex_t mutexTick;
+pthread_cond_t condTick;
+
+int tick = 0;
+
 
 
 int main(void)
 {
+    carros[0].id = 1;
+    carros[0].lin = 4;
+    carros[0].col = 4;
+    carros[0].direcao = 11;
+    carros[0].velocidade = 1;
+    carros[0].Ambulancia = 0;
+    carros[0].thread = 0;
+    carros[0].anterior = '>';
+
     inicializa_mapa();
+    
     while (1) {
         system("clear");
         imprime_mapa();
+        anda_carro(&carros[0]);
         sleep(1);
     }
     return 0;
